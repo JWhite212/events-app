37b43395f75bac72bbdfb20b7fdcc6bdf46b098d
Jamie White <106563693+JWhite212@users.noreply.github.com>
2024-01-07 16:10:06 +0000

    Initial Commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -21,7 +21,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={poppins.variable}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
