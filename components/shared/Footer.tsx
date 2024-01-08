12e41de8fbc4d259f67b08a02a2c6b110d197028
Jamie White <106563693+JWhite212@users.noreply.github.com>
2024-01-08 01:54:39 +0000

    final touches to the header and hero section

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,5 +1,25 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="border-t-">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          {" "}
+          <Image
+            src="/assets/images/logo.svg"
+            height={38}
+            width={128}
+            alt="logo"
+          />{" "}
+        </Link>
+        <p className="text-sm text-gray-500">
+          2024 Events Maximum©. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -9,9 +9,13 @@ const Header = () => {
   return (
     <header className="w-full border-b">
       <div className="wrapper flex items-center justify-between">
-        <Link href="/" className="w-36">
+        <Link
+          href="/"
+          className="w-36">
           <Image
-            src="/assets/images/logo.svg" width={128} height={38}
+            src="/assets/images/logo.svg"
+            width={128}
+            height={38}
             alt="Events Maximum Logo"
           />
         </Link>
@@ -28,16 +32,17 @@ const Header = () => {
             <MobileNav />
           </SignedIn>
           <SignedOut>
-            <Button asChild className="rounded-full" size="lg">
-              <Link href="/sign-in">
-                Login
-              </Link>
+            <Button
+              asChild
+              className="rounded-full"
+              size="lg">
+              <Link href="/sign-in">Login</Link>
             </Button>
           </SignedOut>
         </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
