3cf7ce9d61f8b74cb97db92403eadbf6634db63e
Jamie White <106563693+JWhite212@users.noreply.github.com>
2024-09-30 22:58:40 +0100

    Page Update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
-        <div className="wrapper  grid grid-cols-1 gap-5 md:grid-cols-2">
+        <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2">
           <div className="flex flex-col justify-center gap-8">
             <h1 className="h1-bold">
               Host, Connect, Celebrate: Your Events, Our Platform!
@@ -18,7 +18,7 @@ export default function Home() {
             <Button
               size="lg"
               asChild
-              className="button w-full sm:w-fit">
+              className="w-full sm:w-fit">
               <Link href="#events">Explore Now</Link>
             </Button>
           </div>
